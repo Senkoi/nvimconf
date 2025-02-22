87d384371243826ac69c029ce1b3345417b76af6
Blood <darkwave3137@gmail.com>
2025-02-22 13:33:31 +0800

    add rlaz

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,30 +3,35 @@ return {
         "williamboman/mason.nvim",
         config = function()
             -- require("config.lsp")
-            require("mason").setup()
+            require("mason").setup({
+                registries = {
+                    'github:mason-org/mason-registry',
+                    'github:crashdummyy/mason-registry',
+                },
+            })
         end,
     },
     {
 
         "williamboman/mason-lspconfig.nvim",
-         config = function()
-             require("mason-lspconfig").setup({
+        config = function()
+            require("mason-lspconfig").setup({
                 ensure_installed = { "lua_ls", "omnisharp", "clangd" }
-             })
-             require("config.mason")
-         end,
+            })
+            require("config.mason")
+        end,
     },
     {
-    	"neovim/nvim-lspconfig",
-    	config = function()
+        "neovim/nvim-lspconfig",
+        config = function()
             -- local lspconf = require('config.lsp')
-    		-- require("config.lsp")
+            -- require("config.lsp")
             -- local lspconfig = require("lspconfig")
             -- lspconfig.omnisharp.setup({})
             -- lspconfig.lua_ls.setup({
-                -- on_attach = lspconf.custom_attach,
+            -- on_attach = lspconf.custom_attach,
             -- })
             -- require("config.lsp")
-    	end,
+        end,
     },
 }
